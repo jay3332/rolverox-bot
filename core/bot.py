f9c4aad368ea193aeacbe3525a475732216a07d5
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-05-01 14:22:29 -0700

    Load cogs

--- core/bot.py
+++ core/bot.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+
 import jishaku
 from discord import Intents
 from discord.ext.commands import Bot
@@ -16,6 +18,16 @@ class Rolverox(Bot):
             command_prefix='r!',
             intents=Intents.all()
         )
+
+    async def load_all_extensions(self) -> None:
+        for file in os.listdir('./cogs'):
+            if file.endswith('.py'):
+                await self.load_extension(f'cogs.{file[:-3]}')
+
+        await self.load_extension('jishaku')
+    
+    async def on_ready(self) -> None:
+        print(f'Logged in as {str(self.user)}')
     
     def run(self) -> None:
         super().run(token=config.token)
