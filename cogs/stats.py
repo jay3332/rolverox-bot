342ff1b950df957940aa68bcc9822118c5d4ac76
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-05-01 15:21:55 -0700

    Stats

---
+++ cogs/stats.py
@@ -0,0 +1,18 @@
+from discord import TextChannel
+from discord.ext.tasks import loop
+
+from core.cog import Cog
+
+class Stats(Cog):
+    @loop(minutes=5)
+    async def update_stats(self) -> None:
+        guild = self.bot.get_guild(969411991506661449)
+        assert guild is not None
+
+        members_channel = self.bot.get_channel(970424062360842291)
+        if members_channel is not None:
+            assert isinstance(members_channel, TextChannel)
+
+            await members_channel.edit(name=f'members-{guild.member_count}')
+
+setup = Stats.setup
--- core/cog.py
+++ core/cog.py
@@ -1,16 +1,90 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import asyncio
+import logging
+from typing import TYPE_CHECKING, Any
 
-from discord.ext.commands import Cog as _Cog
+from discord.ext.commands import Cog as _Cog, CogMeta
+from discord.ext.tasks import Loop
 
 if TYPE_CHECKING:
     from .bot import Rolverox
+    from typing_extensions import Self
+
+__log__ = logging.getLogger(__name__)
+
+class CogMeta(CogMeta):
+    if TYPE_CHECKING:
+        __tasks__: list[Loop]
 
 
-class Cog(_Cog):
+class MetaTask(CogMeta):
+    """
+    A simple Metclass that can be used to get all tasks.Loop from the class,
+    to start and cancel them easily.
+    """
+    def __new__(cls, name: Any, bases: Any, attrs: Any, **kwargs: Any) -> Self:
+        new_cls = super().__new__(cls, name, bases, attrs)
+        _inner_tasks = []
+
+        for _, value in attrs.items():
+            if issubclass(value.__class__, Loop):
+                _inner_tasks.append(value)
+
+        new_cls.__tasks__ = _inner_tasks  # type: ignore
+
+        return new_cls
+
+    def _unload_tasks(cls) -> None:
+        for task in cls.__tasks__:
+            coro = task.__dict__.get('coro')
+            if not coro:
+                continue
+
+            __log__.info(
+                f'Stopping task {coro.__name__} after {task.current_loop} intervals.'
+            )
+
+            loop = asyncio.get_running_loop()
+            _tasks = []
+
+            if task.is_running():
+                task.cancel()
+                _tasks.append(task._task)
+
+            loop.create_task(asyncio.gather(*_tasks))  # type: ignore
+
+    def _load_tasks(cls, self) -> None:
+        for task in cls.__tasks__:
+            coro = task.__dict__.get('coro')
+
+            if not coro:
+                continue
+
+            __log__.info(
+                f'Stopping task {coro.__name__} after {task.current_loop} intervals.'
+            )
+
+            if not task.is_running():
+                task.start(self)
+
+
+class Cog(_Cog, metaclass=MetaTask):
+    if TYPE_CHECKING:
+        def _load_tasks(self) -> None:
+            ...
+        
+        def _unload_tasks(self) -> None:
+            ...
+
     def __init__(self, bot: Rolverox) -> None:
         self.bot = bot
+    
+    def cog_load(self) -> None:
+        self._load_tasks()
+    
+    def cog_unload(self) -> None:
+        self._unload_tasks()
     
     @classmethod
     async def setup(cls, bot: Rolverox) -> None:
